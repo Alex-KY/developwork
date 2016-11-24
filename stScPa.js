77f9304fd3d4dbaa6cbc777fddf453458b275471
Alexey <kravchenko_ay@mail.ru>
2016-11-25 01:27:57 +0400

    Update(game 0.1)

--- stScPa.js
+++ stScPa.js
@@ -1,18 +1,33 @@
 var promptButton = document.getElementById("myButton");
 promptButton.addEventListener("click", callPrompt);
+
+var popUp = document.getElementById('pop-up');
 function callPrompt () {
 //var sum1, sum2;
 //while (confirm("Begin?")) {
+
+// selection use weapon
+//popUp.style.display = 'block';
 while (true) {
-	var weap1 = +Math.floor(prompt('Change your weapon ' +
+	var weap1 = prompt('Change your weapon ' +
 		'(1 - Stone, ' +
 		'2 - Scissors, '+
-		'3 - Paper)', ''));
-	go:
-	if (weap1<1 || weap1>3) {
-		weap1 = +prompt('Enter an integer from 1 to 3', '');
-		break go;
+		'3 - Paper)', '');
+
+	if (weap1 == null) {
+		break;
 	}
+
+// value verification
+go:	
+	while (weap1 < 1 || weap1 > 3 || isNaN(weap1) == true){
+		if (weap1 == null) break;
+		weap1 = prompt('Enter an integer from 1 to 3', '');
+		continue go;
+	}
+
+// game
+	Math.floor(weap1);
 	var weap2 = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 	var win1 = win(weap1, weap2);
 	if (win1 == null) {
@@ -21,11 +36,15 @@ while (true) {
 	} else if (win1) {
 		alert("You are a Winner! Get one point!");
 		break;
-	} else alert("You lose! Score one for the winner");
+	} else if (weap1 == null) {
+		break;
+	} else alert("You lose! Score one for the winner")
 		break;
 
 }
 }
+
+
 function win(weap1, weap2) {
 		if (weap1 == weap2)
 			return null;
